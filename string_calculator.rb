bc15bb609764ccd5d34ceb3ffafebb74fdfa001e
Vikas Waktariya <vikasdhakad989@gmail.com>
2024-05-06 23:30:03 +0530

    Step 3: Allow the add method to handle new lines between numbers.

--- string_calculator.rb
+++ string_calculator.rb
@@ -2,7 +2,10 @@ def add(*input_numbers)
   return 0 if input_numbers.empty?
 
   input_numbers = input_numbers.join(',') if input_numbers.is_a?(Array)
-  input_numbers.split(',').map(&:to_i).sum
+  if input_numbers.include?(",\n")
+    return "Invalid Input Format: Comma followed by a new line."
+  end
+  input_numbers.gsub("\n", ",").split(',').map(&:to_i).sum
 end
 
 # Basic Test Case
@@ -12,3 +15,7 @@ numbers = add('1,5')
 
 # Any amount of numbers test case
 add('1,5,5', '6,7,4', '2,1')
+
+# Handle new lines between numbers
+add("1\n2,3")
+add("1,\n")
