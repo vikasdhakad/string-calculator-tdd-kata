c33fed70146afb2fed22665be11778bd795576d7
Vikas Waktariya <vikasdhakad989@gmail.com>
2024-05-06 23:21:30 +0530

    Step 2: Allow the add method to handle an any amount of numbers.

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,10 +1,14 @@
-def add(input_numbers)
+def add(*input_numbers)
   return 0 if input_numbers.empty?
 
+  input_numbers = input_numbers.join(',') if input_numbers.is_a?(Array)
   input_numbers.split(',').map(&:to_i).sum
 end
 
-# Basic Test Case 
+# Basic Test Case
 numbers = add('')
 numbers = add('1')
 numbers = add('1,5')
+
+# Any amount of numbers test case
+add('1,5,5', '6,7,4', '2,1')
