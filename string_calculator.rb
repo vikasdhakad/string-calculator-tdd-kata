734e7a225bfaa778c2c1ac328449f1432f5ee213
Vikas Waktariya <vikasdhakad989@gmail.com>
2024-05-07 00:06:17 +0530

    Step 4: Support different delimiters.

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,11 +1,25 @@
 def add(*input_numbers)
   return 0 if input_numbers.empty?
 
-  input_numbers = input_numbers.join(',') if input_numbers.is_a?(Array)
+  input_numbers = handle_different_delimiters(input_numbers)
   if input_numbers.include?(",\n")
     return "Invalid Input Format: Comma followed by a new line."
   end
   input_numbers.gsub("\n", ",").split(',').map(&:to_i).sum
+end
+
+def handle_different_delimiters(input_numbers)
+  updated_input_numbers = []
+  input_numbers.each do |input_number|
+    if input_number.start_with?("//")
+      delimiter_end_index = input_number.index("\n")
+      delimiter = input_number[2..delimiter_end_index - 1]
+      updated_input_numbers << input_number[delimiter_end_index + 1..-1].gsub(delimiter, ",")
+    else
+      updated_input_numbers << input_number
+    end
+  end
+  updated_input_numbers.join(',')
 end
 
 # Basic Test Case
@@ -19,3 +33,6 @@ add('1,5,5', '6,7,4', '2,1')
 # Handle new lines between numbers
 add("1\n2,3")
 add("1,\n")
+
+# Support different delimiters
+add("//;\n1;2")
